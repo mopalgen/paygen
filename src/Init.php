6c115879b9a78e042f21576ab2e1dcfac9ac66ce
Ajinkya Dhande <ajinkya@mopalgen.com>
2024-06-13 00:01:07 +0530

    Update Init.php to return response

--- src/Init.php
+++ src/Init.php
@@ -19,24 +19,24 @@ class Init
     public function createOrder($order_details, $payment_method, $payment_token, $transaction_session_id = "")
     {
         $processor = new Action();
-        $processor->sale($this->_securityToken, $this->_customer, $order_details, $payment_method, $payment_token, $transaction_session_id);
+        return $processor->sale($this->_securityToken, $this->_customer, $order_details, $payment_method, $payment_token, $transaction_session_id);
     }
 
     public function createOrderUsingSavedCard($order_details, $payment_method, $customer_vault_id, $transaction_session_id = "")
     {
         $processor = new Action();
-        $processor->saleUsingCustomerVault($this->_securityToken, $this->_customer, $order_details, $payment_method, $customer_vault_id, $transaction_session_id);
+        return $processor->saleUsingCustomerVault($this->_securityToken, $this->_customer, $order_details, $payment_method, $customer_vault_id, $transaction_session_id);
     }
 
     public function createRefund($transaction_id, $amount)
     {
         $processor = new Action();
-        $processor->refund($this->_securityToken, $transaction_id, $amount);
+        return $processor->refund($this->_securityToken, $transaction_id, $amount);
     }
 
     public function cancelOrder($transaction_id)
     {
         $processor = new Action();
-        $processor->void($this->_securityToken, $transaction_id);
+        return $processor->void($this->_securityToken, $transaction_id);
     }
 }
